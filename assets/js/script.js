65f4da145db00f4bc4889f52fb09463e148d13d0
Julio <julio_8002@yahoo.com>
2021-03-20 13:31:02 -0500

    A test script was added which gets movie information from OMDB

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var charaInfoEl = $('#characterInfo');
 var homeDescEl = $('.homeDesc');
 
 // Get value from drop-down list
-characters.submit(function (event){
+characters.submit(function(event) {
     event.preventDefault();
 
     var charaChoice = charaInput.val();
@@ -20,22 +20,22 @@ function searchApi(charaChoice) {
     var locQueryUrl = 'https://gateway.marvel.com:443/v1/public/characters?name=';
 
     locQueryUrl = locQueryUrl + charaChoice + '&ts=1&apikey=a37a09ef4c6e1092fd787cc4f25a802c&hash=26e79fcd0a3f7d62a4afad761c48c24a';
-  
+
     fetch(locQueryUrl)
-      .then(function (response) {
-        if (!response.ok) {
-          throw response.json();
-        }
-  
-        return response.json();
-      })
-      .then(displayInfo)
-      .catch(function (error) {
-        console.error(error);
-      });
+        .then(function(response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+
+            return response.json();
+        })
+        .then(displayInfo)
+        .catch(function(error) {
+            console.error(error);
+        });
 };
 
-function displayInfo (locRes) {
+function displayInfo(locRes) {
     // Remove homepage description
     homeDescEl.remove();
     // write query to page so user knows what they are viewing
@@ -44,7 +44,7 @@ function displayInfo (locRes) {
     // resultTextEl.textContent = locRes.search.query;
     charaInfoEl.html('<img src=' + results.thumbnail.path + '.' + results.thumbnail.extension + '></img><h1>' + results.name + '</h1><p>' + results.description + '</p>');
 
-    charaInfoEl.append($('<h3>Comics ' +  + ' has appeared in:</h3>'));
+    charaInfoEl.append($('<h3>Comics ' + +' has appeared in:</h3>'));
     var comics = results.series.items
     var comicsList = $('<ul></ul>');
     for (var i = 0; i < 6; i++) {
@@ -62,3 +62,20 @@ function displayInfo (locRes) {
 
 
 // Display movies on screen
+
+//This is Julios-Prototype - Connecting to OMDB web site and pulling movie information
+// Out of the Movie object we want to extract, Movie Poster, Plot, Rating
+// In the script you'll find my key - which is: 693e75d2
+
+// var randomMovieArray =["Wolverine","Logan","X-Man","X2"]
+var movie = "Wolverine";
+console.log(movie)
+
+function movieApiCall() {
+    $.getJSON('http://www.omdbapi.com/?t=Wolverine&apikey=693e75d2').then(function(response) {
+        console.log(response);
+    })
+}
+movieApiCall();
+
+// http: //www.omdbapi.com/?i=tt3896198&apikey=693e75d2
